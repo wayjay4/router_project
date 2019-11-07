66108e84d373068bb2c2eec6f0abdabefb8ab40c
Waylon Dixon <wdixon@waylondixon.com>
2019-11-07 14:46:20 -0700

    added and implemented useState and useEffect components for API call to sw_API

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Shop(){
+  // declare fetchItems function
+  const fetchItems = async () => {
+    // set swapi api_url
+    const api_url = "https://swapi.co/api/people/";
+
+    // get data from api_url
+    const data = await fetch(api_url);
+
+    // decode recieved jsonData str to items
+    const items = await data.json();
+
+    // save results array to state
+    setItems(items.results);
+
+    // display data for troubleshooting
+    //console.log("items:");
+    //console.dir(items);
+    //console.dir(items.results);
+  }
+
+  // execute fetchItems
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  // declare the state
+  const [items, setItems] = useState([]);
+
+  // map the fetched 'items' array to the var shopItems
+  const shopItems = items.map((item, index) => (
+    <p key={index}>{item.name} (sex: {item.gender})</p>
+  ));
+
   return (
     <div className="container">
       <header>
         <h1>This is the shop page</h1>
       </header>
+
+      <div className="shop-item">
+        {
+          // display shopItems
+          shopItems
+        }
+      </div>
     </div>
   );
 }
