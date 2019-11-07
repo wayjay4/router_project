e5606cc068e2831fe27437dbe1d6249c7c326de3
Waylon Dixon <wdixon@waylondixon.com>
2019-11-07 13:25:31 -0700

    added Link component and addtl navlink styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Navigation(){
   return (
     <nav className="navigation-bar">
       <h3>Logo</h3>
-      <ul>
-        <li className="nav_links">Home</li>
-        <li className="nav_links">About</li>
-        <li className="nav_links">Shop</li>
+      <ul className="nav-links">
+        <Link to="/"><li className="nav_link">Home</li></Link>
+        <Link to="/about"><li className="nav_link">About</li></Link>
+        <Link to="/shop"><li className="nav_link">Shop</li></Link>
       </ul>
     </nav>
   );
